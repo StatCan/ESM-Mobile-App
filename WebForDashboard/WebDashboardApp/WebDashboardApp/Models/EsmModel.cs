a7a194c003f2f375396d7f30aad15b038b2aaa8b
jiandong-guo <guojiandong@hotmail.com>
2020-01-01 20:20:56 -0500

    add db

---
+++ WebForDashboard/WebDashboardApp/WebDashboardApp/Models/EsmModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDashboardApp.Models
+{
+    public class EsmModel
+    {
+        public int Id { get; set; }
+        public Questionnair Questionnair { get; set; }
+        public List<Choice> Options { get; set; }
+        public Answer Answer { get; set; }
+    }
+}
---
+++ WebForDashboard/WebDashboardApp/WebDashboardApp/Models/QuestionModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDashboardApp.Models
+{
+    public class QuestionModel
+    {
+        public int QuestionnairId { get; set; }
+        public string Text { get; set; }
+        public string UserToken { get; set; }
+        public string NotificationId { get; set; }
+        public List<ChoiceModel> Choices { get; set; }
+        public string Message { get; set; }
+        public int Type { get; set; }
+        public string Culture { get; set; }
+    }
+    public class ChoiceModel
+    {
+        public int ChoiceId { get; set; }
+        public string Text { get; set; }
+        public bool IsUserInput { get; set; }
+    }
+    public class AnswerModel
+    {
+        public int QuestionnairId { get; set; }
+        public string UserToken { get; set; }
+        public string NotificationId { get; set; }
+        public int ChoiceId { get; set; }
+        public string UserInput { get; set; }
+    }
+}
