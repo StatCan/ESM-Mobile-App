683b2c56371fbcadeba0f36bb8796f1fc21f2020
jiandong-guo <guojiandong@hotmail.com>
2020-01-09 15:42:25 -0500

    Test background image

--- ReactNativeExpo/components/Homescreen.js
+++ ReactNativeExpo/components/Homescreen.js
@@ -1,17 +1,20 @@
 // Homescreen.js
 import React, { Component } from 'react';
-import { Button, View, Text, TextInput, Image, StyleSheet } from 'react-native';
+import { Button, View, Text, TextInput, Image, StyleSheet,ImageBackground,Dimensions } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 interface State { userName: string; password: string; }
+const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get('window').width;
 
 export default class Homescreen extends Component {
   checkTimeOut = () => { if (Date.now() - global.timeStamp > 10000) return true; else return false; }
   render() {
     return (
+    <ImageBackground resizeMode="repeat" source={require('./background.png')} style={styles.background} resizeMode='stretch'>
+      <Image source={require('./StatCanLogo.png')} style={{ width: null, height: 100 }} />
       <View style={styles.homeContainer}>
-        <Image source={require('./StatCanLogo.png')} style={styles.logo} />
         <View style={styles.homeButtonContainer}>
           <View style={styles.homeButtonColumn}>
             <Button title={resources.getString('start_survey')} style={styles.homeButton} onPress={() => this.props.navigation.navigate('CurrentEQ')} />
@@ -25,13 +28,14 @@ export default class Homescreen extends Component {
             <Button title={resources.getString('contact_us')} style={styles.homeButton} onPress={() => this.props.navigation.navigate('ContactUs')} />
           </View>
         </View>
-         <Button title={"Test"} style={styles.homeButton} onPress={() => this.props.navigation.navigate('WebScreen')} />
       </View>
+      </ImageBackground>
     )
   }
 }
 const styles = StyleSheet.create({
-  homeContainer: { flex: 1, alignItems: 'center', justifyContent: 'space-between' },
+  background: {flex: 1, width: deviceWidth,height: null,},
+  homeContainer: { flex: 1, alignItems: 'center', justifyContent: 'space-between',marginTop:40 },
   logo: { width: 300, height: 100 },
   homeButtonContainer: { flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignContent: 'space-between' },
   homeButtonColumn: { width: 150, height: 150, justifyContent: 'space-between', alignContent: 'space-between' },
